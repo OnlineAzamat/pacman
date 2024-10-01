ed658446f76f67e1c2b0d190faa4309d9e7a8a87
OnlineAzamat <yakubbaev.azamat@mail.ru>
2024-10-01 17:01:47 +0500

    Create default functions: draw, update, gameLoop and other

--- game.js
+++ game.js
@@ -8,6 +8,12 @@ let createRect = (x, y, width, height, color) => {
   canvasContext.fillStyle = color;
   canvasContext.fillRect(x, y, width, height);
 };
+let fps = 30;
+let oneBlockSize = 20;
+let wallColor = "#342DCA";
+let wallSpaceWidth = oneBlockSize / 1.3;
+let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
+let wallInnerColor = "black";
 
 let map = [
   [ 1,1,1,1,1 ,1,1,1,1,1 ,1, 1,1,1,1, 1,1,1,1,1, 1 ],
@@ -33,4 +39,35 @@ let map = [
   [ 1,2,1,1,1 ,1,1,1,1,2 ,1, 2,1,1,1, 1,1,1,1,2, 1 ],
   [ 1,2,2,2,2 ,2,2,2,2,2 ,2, 2,2,2,2, 2,2,2,2,2, 1 ],
   [ 1,1,1,1,1 ,1,1,1,1,1 ,1, 1,1,1,1, 1,1,1,1,1, 1 ],
-]
+];
+
+let gameLoop = () => {
+  update();
+  draw();
+}
+
+let update = () => {
+  // todo
+}
+
+let draw = () => {
+  createRect(0, 0, canvas.width, canvas.height, "black");
+  // todo
+  drawWalls()
+}
+
+let gameInterval = setInterval(gameLoop, 1000/fps);
+
+let drawWalls = () => {
+  for(let i = 0; i < map.length; i++) {
+    for(let j = 0; j < map[0].length; j++) {
+      if(map[i][j] == 1) { // then it is a wall
+        createRect(j * oneBlockSize, i * oneBlockSize, oneBlockSize, oneBlockSize, wallColor)
+      }
+
+      if(j > 0 && map[i][j - 1] == 1) {
+        createRect(j * oneBlockSize, i * oneBlockSize + wallOffset, wallSpaceWidth + wallOffset, wallSpaceWidth, wallInnerColor);
+      }
+    }
+  }
+}
