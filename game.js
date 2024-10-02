8d9efd90bf3d445255b41b86290205a02f3c78e7
OnlineAzamat <yakubbaev.azamat@mail.ru>
2024-10-02 22:12:03 +0500

    Complete map structure and start pacman.js

--- game.js
+++ game.js
@@ -11,7 +11,7 @@ let createRect = (x, y, width, height, color) => {
 let fps = 30;
 let oneBlockSize = 20;
 let wallColor = "#342DCA";
-let wallSpaceWidth = oneBlockSize / 1.3;
+let wallSpaceWidth = oneBlockSize / 1.5;
 let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
 let wallInnerColor = "black";
 
@@ -62,12 +62,54 @@ let drawWalls = () => {
   for(let i = 0; i < map.length; i++) {
     for(let j = 0; j < map[0].length; j++) {
       if(map[i][j] == 1) { // then it is a wall
-        createRect(j * oneBlockSize, i * oneBlockSize, oneBlockSize, oneBlockSize, wallColor)
+        createRect(
+          j * oneBlockSize, 
+          i * oneBlockSize, 
+          oneBlockSize, 
+          oneBlockSize, 
+          wallColor
+        )
       }
 
       if(j > 0 && map[i][j - 1] == 1) {
-        createRect(j * oneBlockSize, i * oneBlockSize + wallOffset, wallSpaceWidth + wallOffset, wallSpaceWidth, wallInnerColor);
+        createRect(
+          j * oneBlockSize, 
+          i * oneBlockSize + wallOffset, 
+          wallSpaceWidth + wallOffset, 
+          wallSpaceWidth, 
+          wallInnerColor
+        );
       }
+
+      if(j < map[0].length - 1 && map[i][j + 1] == 1) {
+        createRect(
+          j * oneBlockSize + wallOffset,
+          i * oneBlockSize + wallOffset,
+          wallSpaceWidth + wallOffset,
+          wallSpaceWidth,
+          wallInnerColor
+        )
+      };
+
+      if(i > 0 && map[i - 1][j] == 1) {
+        createRect(
+          j * oneBlockSize + wallOffset, 
+          i * oneBlockSize, 
+          wallSpaceWidth, 
+          wallSpaceWidth + wallOffset, 
+          wallInnerColor
+        );
+      }
+
+      if(i < map.length - 1 && map[i + 1][j] == 1) {
+        createRect(
+          j * oneBlockSize + wallOffset,
+          i * oneBlockSize + wallOffset,
+          wallSpaceWidth,
+          wallSpaceWidth + wallOffset,
+          wallInnerColor
+        )
+      };
     }
   }
 }
