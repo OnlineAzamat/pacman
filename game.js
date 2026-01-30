52b69edf9bdfb9ceb24aa9761422c558e07efcef
onlineazamat <yakubbaev.azamat@mail.ru>
2026-01-30 20:34:07 +0500

    Implement food consumption logic and update score in Pacman game

--- game.js
+++ game.js
@@ -14,6 +14,8 @@ let wallColor = "#342DCA";
 let wallSpaceWidth = oneBlockSize / 1.5;
 let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
 let wallInnerColor = "black";
+let foodColor = "#feb897";
+let score = 0;
 
 const DIRECTION_RIGHT = 4;
 const DIRECTION_UP = 3;
@@ -52,19 +54,28 @@ let gameLoop = () => {
 }
 
 let update = () => {
-  // todo
-  pacman.moveProcess()
+  pacman.moveProcess();
+  pacman.eat();
 }
 
 let drawFoods = () => {
   for(let i = 0; i < map.length; i++) {
-    // will edit
+    for (let j = 0; j < map[0].length; j++) {
+      if (map[i][j] == 2) {
+        createRect(
+          j * oneBlockSize + oneBlockSize / 3,
+          i * oneBlockSize + oneBlockSize / 3,
+          oneBlockSize / 3,
+          oneBlockSize / 3,
+          foodColor
+        );
+      }
+    }
   }
 }
 
 let draw = () => {
   createRect(0, 0, canvas.width, canvas.height, "black");
-  // todo
   drawWalls()
   drawFoods();
   pacman.draw();
--- pacman.js
+++ pacman.js
@@ -23,6 +23,21 @@ class Pacman {
     }
   }
 
+  eat() {
+    for (let i = 0; i < map.length; i++) {
+      for (let j = 0; j < map[0].length; j++) {
+        if (
+          map[i][j] == 2 &&
+          this.getMapX() == j &&
+          this.getMapY() == i
+        ) {
+          map[i][j] = 3;
+          score++;
+        }
+      }
+    }
+  }
+
   moveBackwards() {
     switch(this.direction) {
       case DIRECTION_RIGHT:
